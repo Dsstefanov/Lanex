d2ba31d048ecc8d9be85cc8eea9edccd48c24de2
Atanas Bosnov <abosnov@gmail.com>
2017-03-20 10:09:21 +0100

    Created the Employee and Contractor class in the ModelLayer

---
+++ src/ModelLayer/Employee.java
@@ -0,0 +1,10 @@
+package ModelLayer;
+
+/**
+ * Created by Yeah on 3/20/2017.
+ */
+public class Employee extends Person {
+    public Employee(String name, String address, String email, String phone, String city) {
+        super(name, address, email, phone, city);
+    }
+}
