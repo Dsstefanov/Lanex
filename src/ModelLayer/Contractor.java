79ff879980e0b44e0059444e24fa1c18e8653390
stefan <amxx@abv.bg>
2017-04-29 11:55:52 +0200

    Contractor almost done

--- src/ModelLayer/Contractor.java
+++ src/ModelLayer/Contractor.java
@@ -4,11 +4,22 @@ package ModelLayer;
  * Created by Yeah on 3/20/2017.
  */
 public class Contractor extends Person {
-
     private int cvr;
+
+    public Contractor() {
+        super(null, null, null, null, null);
+    }
 
     public Contractor(String name, String address, String email, String phone, String city, int cvr) {
         super(name, address, email, phone, city);
+        this.cvr = cvr;
+    }
+
+    public int getCvr() {
+        return cvr;
+    }
+
+    public void setCvr(int cvr) {
         this.cvr = cvr;
     }
 }
