3628f8f8f25cf08d14ac1f966771d46eb744dace
stefan <amxx@abv.bg>
2017-05-08 22:24:32 +0200

    Contractor fix update method + test cases

--- src/DBLayer/IDBContractor.java
+++ src/DBLayer/IDBContractor.java
@@ -9,10 +9,10 @@ import java.util.ArrayList;
 /**
  * Created by Admin on 5/8/2017.
  */
-public interface DBIFContractor {
+public interface IDBContractor {
     Contractor create(String name, String address, String email, String phone, String city, int cvr) throws SQLException;
     Contractor read(int cvr) throws SQLException;
-    Contractor update(int id, String name) throws SQLException;
+    boolean update(Contractor contractor, int cvr) throws SQLException;
     boolean delete(int id) throws SQLException;
     ArrayList<Person> readAll() throws SQLException;
 }
