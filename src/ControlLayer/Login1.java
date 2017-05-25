122a1db89ad14bd8653908514187fd17cf49cde4
Luke <poortrolls@gmail.com>
2017-05-25 11:57:30 +0200

    login

--- src/ControlLayer/Login1.java
+++ src/ControlLayer/Login1.java
@@ -1,19 +1,23 @@
 package ControlLayer;
 import DBLayer.DBConnection;
-import ModelLayer.Employee;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 /**
  * Created by Luke on 23.05.2017.
  */
-public class Login {
-    ArrayList<String> logins = new ArrayList<>();
+public class Login1 {
+    ArrayList<Integer> logins = new ArrayList<>();
+
+
+    public Login1() {
+        setup();
+    }
 
     public void setup() {
 
-        ArrayList<String> logins = new ArrayList<>();
+
         PreparedStatement ps = null;
         String sql = String.format("SELECT WORK_ID FROM EMPLOYEE");
         try {
@@ -24,7 +28,7 @@ public class Login {
 
 
             while (rs.next()) {
-                logins.add(rs.getString(1));
+                logins.add(rs.getInt(1));
 
             }
 
@@ -34,14 +38,17 @@ public class Login {
         }
     }
 
-    public boolean checkLogin(String log) {
+
+
+    public boolean checkLogin(Integer x) {
         boolean loginSuccessful = false;
         try {
 
 
-            if (logins.contains(log)) {
+            if (logins.contains(x)) {
                 loginSuccessful = true;
             }
+
 
         } catch (Exception e) {
             e.printStackTrace();
