b5a379e21fb1a51f112328a04a870561c6c678b2
Dimitar Stefanov <dsstefanov2@gmail.com>
2017-05-29 20:58:39 +0200

    including the arraylist holding nearly out of stock products

--- src/ControlLayer/NotificationController.java
+++ src/ControlLayer/NotificationController.java
@@ -8,16 +8,22 @@ import java.util.ArrayList;
  * Created by USER on 29.5.2017 г..
  */
 public class NotificationController {
+    /**
+     * productsToOrder: arraylist of all products that needs to be ordered
+     */
     ArrayList<Product> productsToOrder = new ArrayList<>();
+    ArrayList<Product> runningOutOfStockSoon = new ArrayList<>();
     ProductController productController = new ProductController();
     public boolean substractAvarageConsumption(){
         for (Product product:productController.readAll()) {
-            if(product.getCurrentQuantity()-product.getDailyConsumption()<0){
+            if(product.getCurrentQuantity()<product.getDailyConsumption()){
                 //TODO create an exception and throw it
             }else{
                 product.setCurrentQuantity(product.getCurrentQuantity()-product.getDailyConsumption());
                 if (product.getCurrentQuantity()<=product.getMinQuantity()){
                     productsToOrder.add(product);
+                }else if(product.getCurrentQuantity()<=product.getMinQuantity()+product.getMinQuantity()*0.1){//*0.1: 10% above the minimum quantity
+                    runningOutOfStockSoon.add(product);
                 }
             }
         }
