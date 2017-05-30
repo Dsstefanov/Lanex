63fad1caef90d8610fbd56bfea717d874caae300
Luke <poortrolls@gmail.com>
2017-05-31 00:01:59 +0200

    notification implementation

--- src/ControlLayer/NotificationController.java
+++ src/ControlLayer/NotificationController.java
@@ -1,19 +1,54 @@
 package ControlLayer;
 
+import DBLayer.DBConnection;
+import DBLayer.DBNotification;
 import ModelLayer.Product;
 
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
 
 /**
  * Created by USER on 29.5.2017 г..
  */
-public class NotificationController {
+public class NotificationController extends Timer {
     /**
      * productsToOrder: arraylist of all products that needs to be ordered
      */
     ArrayList<Product> productsToOrder = new ArrayList<>();
     ArrayList<Product> runningOutOfStockSoon = new ArrayList<>();
     ProductController productController = new ProductController();
+    String lastUpdated;
+    Date lastUpdatedDate;
+    String today;
+    DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
+    boolean update;
+    DBNotification dbNotification = new DBNotification();
+
+
+    public void Compare(){
+        try{
+            Date todayDate = dateFormat.parse(dbNotification.getLastUpdated());
+            Date lastupdateDate = dateFormat.parse(dbNotification.getCurrentDate());
+            if(todayDate.getTime() - lastupdateDate.getTime() > 1) {
+               substractAvarageConsumption();
+            }
+
+
+        }
+        catch (ParseException p){
+            p.printStackTrace();
+        }
+
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
     public boolean substractAvarageConsumption(){
         for (Product product:productController.readAll()) {
             if(product.getCurrentQuantity()<product.getDailyConsumption()){
@@ -24,7 +59,9 @@ public class NotificationController {
                         product.getHeight(), product.getLength(), product.getWidth(), product.getBarcode(),
                         product.getMinQuantity(), product.getMaxQuantity(), product.getCurrentQuantity(),
                         product.getDailyConsumption(), product.getName(), product.getCvr()
+
                 );
+                dbNotification.insertLastUpdatedDate();
                 if(success) {
                     if (product.getCurrentQuantity() <= product.getMinQuantity()) {
                         productsToOrder.add(product);
@@ -50,4 +87,6 @@ public class NotificationController {
             return false;//TODO will be changed to throw exception showing what went wrong
         }
     }
+
+
 }
