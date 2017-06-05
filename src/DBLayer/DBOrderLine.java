09ed05decf8b3ac148e2228740004d83f4c17e6a
redjohn1 <john_4youu@yahoo.com>
2017-06-06 00:43:42 +0200

    IsOrdered is now getting updated when you create the order.

--- src/DBLayer/DBOrderLine.java
+++ src/DBLayer/DBOrderLine.java
@@ -22,7 +22,9 @@ public class DBOrderLine {
             ps1.setInt(3,product.getCurrentQuantity());
             //ps1.setInt(2,0);
             ps1.executeUpdate();
-
+            PreparedStatement ps2 = conn.prepareStatement("UPDATE Product SET isOrdered = 1 WHERE barcode = ?");
+            ps2.setString(1,product.getBarcode());
+            ps2.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
